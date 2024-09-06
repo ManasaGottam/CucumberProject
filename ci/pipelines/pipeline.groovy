e072f4a36f9c97c3166d8f10145c6135fc7aefc4
mgottam <manasa.gottam@oracle.com>
2024-09-06 15:17:34 +0530

    Update files

---
+++ ci/pipelines/pipeline.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent any
+
+    tools {
+        maven "Maven 3.9.4"
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Checkout code from Git repository
+                git url: 'https://github.com/ManasaGottam/CucumberProject.git', branch: 'main'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                // Run Maven build
+                sh 'mvn clean install'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                // Run Cucumber tests
+                sh 'mvn test'
+            }
+        }
+    }
+
+    post {
+        always {
+            // Archive test results
+            junit '**/target/surefire-reports/TEST-*.xml'
+        }
+    }
+}
