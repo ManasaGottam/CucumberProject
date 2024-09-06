9c0c60cff02cd4040745388be75c74cdff655f9e
mgottam <manasa.gottam@oracle.com>
2024-09-06 16:48:01 +0530

    Update files

--- ci/pipelines/pipeline.groovy
+++ ci/pipelines/pipeline.groovy
@@ -16,13 +16,13 @@ pipeline {
         stage('Build') {
             steps {
                 // Run Maven build
-                bat 'mvn clean install'
+                bat 'mvn clean install -DskipTests'
             }
         }
 
         stage('Test') {
             steps {
-                // Run Cucumber tests
+                // Run tests
                 bat 'mvn test'
             }
         }
@@ -32,7 +32,16 @@ pipeline {
         always {
             // Archive test results
 //            junit '**/target/surefire-reports/TEST-*.xml'
+            archiveArtifacts artifacts: '**/*report*/**/*, **/logs/**/*'
             junit 'reports/Session_*/html-report.html'
+            publishHTML (target: [
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: true,
+                    keepAll: true,
+                    reportDir: 'reports/cucumber-reports',
+                    reportFiles: 'cucumberReport.html',
+                    reportName: 'Cucumber HTML Report'
+            ])
         }
     }
 }
